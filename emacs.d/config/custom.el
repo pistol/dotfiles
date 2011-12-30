2c6159969999b8134ddd301e3cff74c82c14ef8b
pistol <pistol@pistol.local>
2011-12-30 02:22:57 -0800

    Further changes to fork. Renamed rmm5t to config and added own emacs 24 theme pistol-theme

---
+++ emacs.d/config/custom.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("cc3aee0c45b7eafc0f4c64c67fb89b066c62e2c1" "56103d9b5654942dacc534ff5511662ffbc1f2ef" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs.d/config/theme.el
@@ -0,0 +1,10 @@
+;;; Theme customization
+
+(load-theme 'wombat)
+;(load-theme 'tango-dark)
+
+;(color-theme-initialize)
+;(vendor 'color-theme-wombat)
+;(color-theme-wombat)
+;(blink-cursor-mode)
+;(set-cursor-color "orange")
