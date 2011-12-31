5b0b52c369248938398c5349d719adce035273c5
pistol <pistol@pistol.local>
2011-12-31 04:51:29 -0800

    Force symlinks in installer

--- install.rb
+++ install.rb
@@ -11,7 +11,7 @@ home = File.expand_path('~')
 Dir['*'].each do |file|
   next if file =~ /install/ || file =~ /README/
   target = File.join(home, ".#{file}")
-  `ln -fns #{File.expand_path file} #{target}`
+  `ln -fvns #{File.expand_path file} #{target}`
 end
 
 `git submodule sync`
