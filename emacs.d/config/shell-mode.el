084eb467d0a59e5e97cc8574a8f5b99f2e60d472
pistol <pistol@duke-cs-136-dhcp-001.cs.duke.edu>
2012-01-15 16:59:41 -0500

    Auto recognize the new bash_ and bashrc_ files as shell mode

--- emacs.d/config/shell-mode.el
+++ emacs.d/config/shell-mode.el
@@ -6,4 +6,16 @@
 (add-to-list 'auto-mode-alist '("bash_local$" . sh-mode))
 (add-to-list 'auto-mode-alist '("bash_completion$" . sh-mode))
 
+(add-to-list 'auto-mode-alist '("bash_aliases_linux$" . sh-mode))
+(add-to-list 'auto-mode-alist '("bash_aliases_mac$" . sh-mode))
+(add-to-list 'auto-mode-alist '("bash_aliases_private$" . sh-mode))
+(add-to-list 'auto-mode-alist '("bash_aliases_private_linux$" . sh-mode))
+(add-to-list 'auto-mode-alist '("bash_aliases_private_mac$" . sh-mode))
+
+(add-to-list 'auto-mode-alist '("bashrc_linux$" . sh-mode))
+(add-to-list 'auto-mode-alist '("bashrc_mac$" . sh-mode))
+(add-to-list 'auto-mode-alist '("bashrc_private$" . sh-mode))
+(add-to-list 'auto-mode-alist '("bashrc_private_linux$" . sh-mode))
+(add-to-list 'auto-mode-alist '("bashrc_private_mac$" . sh-mode))
+
 (add-hook 'shell-mode-hook (lambda () (setq tab-width 8)))
