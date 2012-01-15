4d149adbbc2ab57cee0af38014d24b885eb1f4df
pistol <pistol@duke-cs-136-dhcp-001.cs.duke.edu>
2012-01-15 16:57:55 -0500

    Map dired keys to dired+

--- emacs.d/config/dired.el
+++ emacs.d/config/dired.el
@@ -2,3 +2,7 @@
 
 ;; Allows recursive deletes
 (setq dired-recursive-deletes 'top)
+
+;; Dired+
+(define-key ctl-x-map   "d" 'diredp-dired-files)
+(define-key ctl-x-4-map "d" 'diredp-dired-files-other-window)
