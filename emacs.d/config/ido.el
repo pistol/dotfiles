4131c688b66b9576ded2dc3e544c3fb03942c7a5
pistol <pistol@duke-cs-136-dhcp-001.cs.duke.edu>
2012-01-15 16:59:06 -0500

    Ignore DS files in ido

--- emacs.d/config/ido.el
+++ emacs.d/config/ido.el
@@ -4,3 +4,4 @@
 (setq ido-enable-flex-matching t) ; fuzzy matching is a must have
 (setq ido-enable-last-directory-history nil) ; forget latest selected directory names
 
+(add-to-list 'ido-ignore-files "\.DS_Store")
