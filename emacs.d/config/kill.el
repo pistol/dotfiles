e218061a6a639f098eb5ce41d38ff5f769065d8b
pistol <pistol@duke-cs-136-dhcp-001.cs.duke.edu>
2012-01-16 02:30:32 -0500

    Added a few new submodules.

---
+++ emacs.d/config/kill.el
@@ -0,0 +1,19 @@
+;; Copy/Cut current line using Alt+w and Ctrl+w
+;; http://xahlee.org/emacs/emacs_copy_cut_current_line.html
+
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy the current line."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (progn
+       (message "Current line is copied.")
+       (list (line-beginning-position) (line-beginning-position 2)) ) ) ))
+
+(defadvice kill-region (before slick-copy activate compile)
+  "When called interactively with no active region, cut the current line."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (progn
+       (list (line-beginning-position) (line-beginning-position 2)) ) ) ))
